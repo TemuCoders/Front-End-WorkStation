5c393dca89bfdde0dc77c47286e583eed1a0c8a5
jhonson2021 <u202219657@upc.edu.pe>
2025-11-30 17:15:10 -0500

    fix: desing

--- src/app/shared/presentation/components/layout/layout.ts
+++ src/app/shared/presentation/components/layout/layout.ts
@@ -1,23 +1,41 @@
-import { Component } from '@angular/core'
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
-import {MatButton} from '@angular/material/button';
-import {TranslatePipe} from '@ngx-translate/core';
-import {LanguageSwitcher} from '../language-switcher/language-switcher';
-import {FooterContent} from '../footer-content/footer-content';
-import {MatIconModule} from '@angular/material/icon';
+import { Component, computed, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+import { MatButton } from '@angular/material/button';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LanguageSwitcher } from '../language-switcher/language-switcher';
+import { FooterContent } from '../footer-content/footer-content';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../../../User/infrastructure/auth.service';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, RouterLink, MatToolbarRow, MatToolbar, MatButton, RouterLinkActive,
-    TranslatePipe, LanguageSwitcher, FooterContent, MatIconModule],
+  imports: [
+    RouterOutlet, 
+    RouterLink, 
+    MatToolbarRow, 
+    MatToolbar, 
+    MatButton, 
+    RouterLinkActive,
+    TranslatePipe, 
+    LanguageSwitcher, 
+    FooterContent, 
+    MatIconModule
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
 export class Layout {
-  options = [
-    {link: '/home', label: 'option.home'},
-    {link: '/searching/workspaces', label: 'option.workspaces'},
-    {link: '/profile/1', label: 'option.profile'},
-  ];
+  private authService = inject(AuthService);
+
+  readonly options = computed(() => {
+    const user = this.authService.currentUser();
+    const profileId = user?.id || 1; 
+
+    return [
+      { link: '/home', label: 'option.home' },
+      { link: '/searching/workspaces', label: 'option.workspaces' },
+      { link: `/profile/${profileId}`, label: 'option.profile' },
+    ];
+  });
 }
